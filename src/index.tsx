816d80899fdfe717561d2e344cdecae179ca30f3
Tae Hyeon Jo <hillskuti@naver.com>
2023-04-19 06:45:33 +0900

    Basic Form Elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  return <h1>hi</h1>;
-};
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-// ReactDOM.render(<App />, document.querySelector('#root'));
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
