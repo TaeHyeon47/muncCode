6269d171372c65f950038c824467359fcd9e7cc5
Tae Hyeon Jo <hillskuti@naver.com>
2023-05-01 11:01:48 +0900

    eval과 trycatch문으로 코드 처리(비동기 문법 작성시 에러 발생)

--- src/index.tsx
+++ src/index.tsx
@@ -35,6 +35,12 @@ const App = () => {
     });
 
     setCode(result.outputFiles[0].text);
+
+    try {
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
