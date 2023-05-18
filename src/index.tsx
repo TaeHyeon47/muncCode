2764ab3d7aeb4fa06f7f04b05b300ce213eb794e
Tae Hyeon Jo <hillskuti@naver.com>
2023-05-19 00:04:02 +0900

    코드 컴포넌트 분리

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
   const startService = async () => {
@@ -25,9 +26,6 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
-    // HTML elements 초기화
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -39,28 +37,8 @@ const App = () => {
       },
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-   <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try {
-            eval(event.data);
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-            console.error(err);
-          }
-        }, false);
-      </script>
-    </body>
-   </html>
-  `;
 
   return (
     <div>
@@ -75,12 +53,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title='preview'
-        ref={iframe}
-        sandbox='allow-scripts'
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
