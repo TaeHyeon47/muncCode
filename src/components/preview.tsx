a293378652cc6de764bf29e583872cbac381978b
Tae Hyeon Jo <hillskuti@naver.com>
2023-05-24 22:49:58 +0900

    iframe영역으로 drag가 되지 않는 버그 수정

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useEffect, useRef } from 'react';
 
 interface PreviewProps {
@@ -34,13 +35,15 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      style={{ backgroundColor: 'white' }}
-      title='preview'
-      ref={iframe}
-      sandbox='allow-scripts'
-      srcDoc={html}
-    />
+    <div className='preview-wrapper'>
+      <iframe
+        //   style={{ backgroundColor: 'white' }}
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
