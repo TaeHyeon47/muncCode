9fd9b26b8cced794570f199e294a35de0a205f42
Tae Hyeon Jo <hillskuti@naver.com>
2023-07-02 23:01:30 +0900

    redux 리팩토링(add cell 추가 시 UI 개선)

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -18,7 +18,7 @@ const CellList: React.FC = () => {
   return (
     <div>
       {renderedCells}
-       <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
     </div>
   );
 };
